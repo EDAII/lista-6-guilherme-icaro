bae77ba2d2a46ba72a4a0f049225d341dddcf603
Guilherme Augusto <guilherme.francais@gmail.com>
2018-10-30 08:11:21 -0300

    Corrects bugs into distance calc
    Co-authored-by: Guilherme Augusto <guilherme.francais@gmail.com>
    Co-authored-by: Icaro Oliveira <icarooliv2@gmail.com>

--- build.js
+++ build.js
@@ -12,9 +12,9 @@ class Point {
 
 var points = []
 
-for (let i = 0; i < 10; i++){
-    x_axis = Math.random()*300;
-    y_axis = Math.random()*300;
+for (let i = 0; i < 30; i++){
+    x_axis = Math.random()*450;
+    y_axis = Math.random()*450;
     color = 'cyan'
 
     let point = new Point(x_axis, y_axis, color);
@@ -24,7 +24,7 @@ for (let i = 0; i < 10; i++){
 
 console.log(points)
 
-let responses = bruteForce(points, 10);
+let responses = bruteForce(points, 30);
 let first_element = responses[0];
 let second_element = responses[1];
 let minimal_distance = responses[2];
@@ -37,7 +37,7 @@ points = changeColor(points, first_element, second_element);
 // console.log(second_element);
 // console.log(minimal_distance);
 
-for (let i = 0; i < 10; i++){
+for (let i = 0; i < 30; i++){
     x_axis = points[i].x;
     y_axis = points[i].y;
     color = points[i].color;
@@ -72,18 +72,14 @@ function dist(p1, p2){
     );
 }
 
-function min(x, y){
-    return (x < y)? x : y;
-}
-
 function bruteForce(element_list, n){
     min = 100000000;
-    for(let i = 0; i < n; i++){
-        for(let j = i+1; j < n; j++){
+    for(let i = 0; i < n; ++i){
+        for(let j = i+1; j < n; ++j){
             if (dist(element_list[i], element_list[j]) < min){
                 element_1 = element_list[i];
-                min = dist(element_list[i], element_list[j]);
                 element_2 = element_list[j];
+                min = dist(element_list[i], element_list[j]);
             }
         }
     }
@@ -91,10 +87,67 @@ function bruteForce(element_list, n){
     return [element_1, element_2, min];
 }
 
+function stripClosest(strip, size, d){
+    min = d;
+
+    strip = quick_sort(strip, 0, size) // make quick sort after
+
+    for (let i = 0; i < size; i++){
+        for(let j = i+1; j < size && (strip[j].y - strip[i].y) < min; j++){
+            if (dist(element_list[i], element_list[j]) < min){
+                element_1 = element_list[i];
+                element_2 = element_list[j];
+                min = dist(element_list[i], element_list[j]);
+            }
+        }
+    }
+
+    return [element_1, element_2, min];
+}
+
+function minimun(x, y){
+    return (x < y)? x : y;
+}
+
+function closestUtil(pointsList, n){
+    if (n <= 3) {
+        return bruteForce(pointsList, n);
+    }
+
+    mid = parseInt(n/2);
+
+    midPoint = new Point(
+        pointsList[mid].x,
+        pointsList[mid].y,
+        pointsList[mid].color
+    )
+
+    left = closestUtil(pointsList, mid);
+    right = closestUtil(pointsList + mid, n-mid);
+
+    d = minimun(left, right);
+
+    strip = [];
+    j = 0;
+    for (let i = 0; i < n; i++){
+        if (Math.abs(pointsList[i].x - midPoint.x) < d) {
+            strip.push(pointsList[i])
+        }
+    }
+
+    return minimun(d, stripClosest(strip, j, d));
+}
+
+function closest(pointsList, n){
+    pointsList = quick_sort(pointsList, 0, n, 'x');
+
+    return closestUtil(pointsList, n);
+}
+
 function changeColor(pointsList, first_element, second_element){
     let new_list = []
 
-    for(i = 0; i < 10; i++){
+    for(i = 0; i < 30; i++){
         console.log(pointsList[i])
         if(pointsList[i] == first_element || pointsList[i] == second_element){
             pointsList[i].color = 'red';
@@ -104,4 +157,73 @@ function changeColor(pointsList, first_element, second_element){
     }
 
     return new_list;
+}
+
+function swap(items, firstIndex, secondIndex){
+    var temp = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
+}
+
+function partition(items, left, right, constant) {
+
+    if(constant == 'x'){
+        var pivot = items[Math.floor((right + left) / 2)],
+            i = left,
+            j = right;
+
+
+        while (i <= j) {
+            while (items[i].x < pivot.x) {
+                i++;
+            }
+            console.log(items[i].x);
+            while (items[j].x > pivot.x) {
+                j--;
+            }
+            if (i <= j) {
+                swap(items, i, j);
+                i++;
+                j--;
+            }
+        }
+    } else {
+        var pivot = items[Math.floor((right + left) / 2)],
+            i = left,
+            j = right;
+
+        while (i <= j) {
+            while (items[i].y < pivot.y) {
+                i++;
+            }
+            while (items[j].y > pivot.y) {
+                j--;
+            }
+            if (i <= j) {
+                swap(items, i, j);
+                i++;
+                j--;
+            }
+        }
+    }
+
+    return i;
+}
+
+function quick_sort(items, left, right, constant) {
+    var index;
+    if (items.length > 1) {
+        left = typeof left != "number" ? 0 : left;
+        right = typeof right != "number" ? items.length - 1 : right;
+        index = partition(items, left, right, constant);
+
+        if (left < index - 1) {
+            quick_sort(items, left, index - 1, constant);
+        }
+        if (index < right) {
+            quick_sort(items, index, right, constant);
+        }
+    }
+
+    return items;
 }
